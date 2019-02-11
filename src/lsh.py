9134723b1e923af73af416615763a8a540f62371
amalroy <amalchettisseril@gmail.com>
2019-02-11 23:34:22 +0530

    update

--- src/lsh.py
+++ src/lsh.py
@@ -1,5 +1,6 @@
 # Implement code for Locality Sensitive Hashing here!
 import numpy as np
+from sklearn.neighbors import LSHForest
 def reduce_dim(X,k,w=10):
     n=X.shape[0]
     d=X.shape[1]
@@ -11,6 +12,11 @@ def reduce_dim(X,k,w=10):
             b=np.random.uniform(0,w)
             random_hash[i,j]=np.floor((np.dot(x,r)+b)/w)
     return random_hash
+def predict(X_test,X_train,y_train):
+    clf=LSHForest()
+    clf.fit(X_train,y_train)
+    return clf.predict(X_test)
+
 #def classify(X,w):
 #    X_hash=np.
 
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import lsh
 import bayes
@@ -15,10 +16,12 @@ import projections
 import nn
 def reduce_dim(X,loc):
     k=X.shape[1]
-    for d in range(2,int(np.ceil(k/2)),2):
+    d=2
+    while(d<=int(np.ceil(k/2))):
         fname=loc+'/X_dim_'+str(d)+'.csv'
         X_red=projections.random_proj(X,d)
         np.savetxt(fname,X_red,delimiter=' ')
+        d=d*2
 count_vect = CountVectorizer()
 parser = argparse.ArgumentParser()
 parser.add_argument('--test-data')
@@ -38,7 +41,7 @@ else:
     y=genfromtxt(args.test_label,delimiter=' ')
 X=normalize(X)
 def bayes_train_test(X,y):
-    n_splits=10
+    n_splits=3
     kf = KFold(n_splits,shuffle=True)
     kf.get_n_splits(X)
     accuracy=[]
@@ -126,37 +129,100 @@ def knn(X,y,k):
     print("Test micro F1 Score ::",f1_mi)
     return acc,f1_ma,f1_mi
 if __name__ == '__main__':
-    # The entire code should be able to run from this file!
+    red=0
+    find_pca=False
+    if(find_pca==True):
+        d=2
+        k=X.shape[1]
+        n_run=int(np.log2(k))-1
+        dims=np.zeros(n_run)
+        accuracy=np.zeros(n_run)
+        f1_macro=np.zeros(n_run)
+        f1_micro=np.zeros(n_run)
+        i=0
+        while(d<=int(np.ceil(k/2))):
+            pca=PCA(n_components=d, svd_solver='arpack')
+            X_red=pca.fit(X)
+            X_red=normalize(X)
+            #acc,f1_ma,f1_mi=knn_train_test(X_red.toarray(),y,3)
+            acc,f1_ma,f1_mi=bayes_train_test(X_red,y)
+            #acc,f1_ma,f1_mi=bayes_sklearn(X.toarray(),y)
+            #acc,f1_ma,f1_mi=knn(X.toarray(),y,5)
+            accuracy[i]=acc
+            f1_macro[i]=f1_ma
+            f1_micro[i]=f1_mi
+            dims[i]=d
+            i=i+1
+            d=d*2
+        f=plt.figure()
+        plt.plot(dims,accuracy)
+        plt.xlabel('Dimension')
+        plt.ylabel('Accuracy')
+        plt.savefig('task_7_acc_bayes_'+args.dataset+'.png')
+        f=plt.figure()
+        plt.plot(dims,f1_macro)
+        plt.xlabel('Dimension')
+        plt.ylabel('f1-macro')
+        plt.savefig('task_7_f1_macro_bayes_'+args.dataset+'.png')
+        f=plt.figure()
+        plt.plot(dims,f1_micro)
+        plt.xlabel('Dimension')
+        plt.ylabel('f1-micro')
+        plt.savefig('task_7_f1_micro_bayes_'+args.dataset+'.png')
+        plt.show()
     #bayes_train_test(X,y)
     #print(X)
     #reduce_dim(X,'../data/'+args.dataset)
     #bayes_sklearn(X,y)
     #knn(X,y,5)
-    k=X.shape[1]
-    dims=range(2,int(np.ceil(k/2)),2)
-    n_run=len(dims)
-    accuracy=np.zeros(n_run)
-    f1_macro=np.zeros(n_run)
-    f1_micro=np.zeros(n_run)
-    
-    loc='../data/'+args.dataset
-    for d in dims:
-        fname=loc+'/X_dim_'+str(d)+'.csv'
-        X_red=genfromtxt(fname,delimiter=' ')
-        y=genfromtxt(args.test_label,delimiter=' ')
-        X=normalize(X)
-        #acc,f1_ma,f1_mi=knn_train_test(X_red,y,5)
-        #acc,f1_ma,f1_mi=bayes_train_test(X,y)
-        #acc,f1_ma,f1_mi=bayes_sklearn(X,y)
-        acc,f1_ma,f1_mi=knn(X,y,5)
-        accuracy[int((d-1)/2)]=acc
-        f1_macro[int((d-1)/2)]=f1_ma
-        f1_micro[int((d-1)/2)]=f1_mi
-    plt.plot(dims,accuracy)
-    plt.xlabel('Dimension')
-    plt.ylabel('Accuracy')
-    plt.show()
-    #    X_red=projections.random_proj(X,d)
-    #    acc,f1_ma,f1_mi=knn_train_test(X_red,y,5)
-    #    f1
+    #do computation on original matrix
+    if(red==0):
+        #run required classifier here
+        bayes_train_test(X,y)
+        #knn_train_test(X,y)
+        #bayes_sklearn(X,y)
+        #knn(X,y,5)
+    #do computation on reduced matrix
+    elif(red==1):
+        k=X.shape[1]
+        n_run=int(np.log2(k))-1
+        dims=np.zeros(n_run)
+        accuracy=np.zeros(n_run)
+        f1_macro=np.zeros(n_run)
+        f1_micro=np.zeros(n_run)
+        #
+        loc='../data/'+args.dataset
+        d=2
+        i=0
+        while(d<=int(np.ceil(k/2))):
+            fname=loc+'/X_dim_'+str(d)+'.csv'
+            X_red=genfromtxt(fname,delimiter=' ')
+            y=genfromtxt(args.test_label,delimiter=' ')
+            X=normalize(X)
+            #acc,f1_ma,f1_mi=knn_train_test(X_red.toarray(),y,3)
+            acc,f1_ma,f1_mi=bayes_train_test(X_red,y)
+            #acc,f1_ma,f1_mi=bayes_sklearn(X.toarray(),y)
+            #acc,f1_ma,f1_mi=knn(X.toarray(),y,5)
+            accuracy[i]=acc
+            f1_macro[i]=f1_ma
+            f1_micro[i]=f1_mi
+            dims[i]=d
+            i=i+1
+            d=d*2
+        f=plt.figure()
+        plt.plot(dims,accuracy)
+        plt.xlabel('Dimension')
+        plt.ylabel('Accuracy')
+        plt.savefig('task_3_acc_bayes_'+args.dataset+'.png')
+        f=plt.figure()
+        plt.plot(dims,f1_macro)
+        plt.xlabel('Dimension')
+        plt.ylabel('f1-macro')
+        plt.savefig('task_3_f1_macro_bayes_'+args.dataset+'.png')
+        f=plt.figure()
+        plt.plot(dims,f1_micro)
+        plt.xlabel('Dimension')
+        plt.ylabel('f1-micro')
+        plt.savefig('task_3_f1_micro_bayes_'+args.dataset+'.png')
+        plt.show()
 
